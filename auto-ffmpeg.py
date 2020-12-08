7ea2cbe69d762a4beeaba4bafba83f0001fb6503
osmond1689 <osmond1689@gmail.com>
2020-12-08 17:58:33 +0800

    更新自动转码

--- auto-ffmpeg.py
+++ auto-ffmpeg.py
@@ -7,7 +7,7 @@ def findAllFile(base):
             newdirname=dirname.replace("vod", "vod-play", 1)
             newdirname=dirname.replace("A02", "test2", 1)
             if wjname.endswith('.mp4') or wjname.endswith('.flv'):
-                print ('ffmpeg -i '+wjname+' -b:v 800k -b:a 64k '+os.path.join(newdirname,wjname))
+                print ('ffmpeg -i '+os.path.join(dirname,wjname)+' -b:v 800k -b:a 64k '+os.path.join(newdirname,wjname))
     return wjlist        
 if __name__ == '__main__':
     base=r"D:\app\Vod_Management\vod"
